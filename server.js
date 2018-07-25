5337fc3ec844d0f403d6def078f7488f1b796e4e
Luke <pippoluca90@hotmail.it>
2018-07-25 13:08:06 +0200

    update conf heroku

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-/Install express server
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+//app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/dalila'));
 
 app.get('/*', function(req,res) {
+
+    //res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/dalila/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+// Start the app by listening on the default Heroku port!!
 app.listen(process.env.PORT || 8080);
